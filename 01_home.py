ffef46f3eb8751cd1f1a4d4407bd47aec0b00883
17032 <69780764+17032@users.noreply.github.com>
2021-08-20 14:44:17 +1200

    Create 01_home.py

---
+++ 01_home.py
@@ -0,0 +1,16 @@
+# import everything from tkinter module
+from tkinter import *
+import random
+
+class Sortit:
+    def __init__(self, parent):
+        print("hello world")
+        
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Sort It")
+    something = Sortit(root)
+    root.mainloop()
+
+
